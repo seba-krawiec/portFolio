e37551c08db12c73c28a4e825e6c54156c7ae7a5
Sebastian Krawiec <sebastian.arielk@gmail.com>
2025-11-07 20:44:49 -0300

    add legacy support vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [preact(),
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
+  ],
 })
