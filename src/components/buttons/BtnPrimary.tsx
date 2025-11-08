1ed0f1fc09fea866cc24b82be14f21af0390f431
Sebastian Krawiec <sebastian.arielk@gmail.com>
2025-11-08 19:29:25 -0300

    btn functionality rework wip

--- src/components/buttons/BtnPrimary.tsx
+++ src/components/buttons/BtnPrimary.tsx
@@ -1,7 +1,18 @@
+import type { ReactNode } from "preact/compat";
+import "./buttonsCss/BtnPrimary.scss";
+import type { MouseEventHandler } from "preact";
+
 type BtnProps = {
-  children: ChildNode | string;
+  label: string;
+  icon?: ReactNode | SVGAElement;
+  handleClick: MouseEventHandler<HTMLButtonElement>;
 };
 
-export const BtnPrimary = ({ children }: BtnProps) => {
-  return <button>{children}</button>;
+export const BtnPrimary = ({ label, icon, handleClick }: BtnProps) => {
+  return (
+    <button className="btn" onClick={handleClick}>
+      {icon}
+      {label}
+    </button>
+  );
 };
