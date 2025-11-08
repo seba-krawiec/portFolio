bd3ef87b46308f644a96a852c9071c6721868dc0
Sebastian Krawiec <sebastian.arielk@gmail.com>
2025-11-08 19:29:07 -0300

    add buttons

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -1,7 +1,22 @@
-type NavProps = {
-  children: ChildNode;
-};
+import { BtnPrimary } from "../buttons/BtnPrimary";
+import IconCv from "../../../public/icons/IconCv";
+import "./layoutCss/Nav.scss";
 
-export const Nav = ({ children }: NavProps) => {
-  return <div>{children}Nav</div>;
+export const Nav = () => {
+  return (
+    <div className="navBar">
+      <BtnPrimary label="Inicio" handleClick={() => console.log("click")} />
+      <BtnPrimary
+        label="Experiencia"
+        handleClick={() => console.log("click")}
+      />
+      <BtnPrimary label="Proyectos" handleClick={() => console.log("click")} />
+      <BtnPrimary label="Contacto" handleClick={() => console.log("click")} />
+      <BtnPrimary
+        label="Curriculum"
+        icon={<IconCv fill={"#4F3874"} />}
+        handleClick={() => console.log("click")}
+      />
+    </div>
+  );
 };
