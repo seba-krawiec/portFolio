e35247a782b00fb8e20c232cb7dd17216e13c6c8
Sebastian Krawiec <sebastian.arielk@gmail.com>
2025-11-07 23:50:09 -0300

    removed secondary btn

--- src/components/buttons/BtnSecondary.tsx
+++
@@ -1,7 +0,0 @@
-type BtnProps = {
-  children: ChildNode | string;
-};
-
-export const BtnSecondary = ({ children }: BtnProps) => {
-  return <button className="btn btn--secondary">{children}</button>;
-};
