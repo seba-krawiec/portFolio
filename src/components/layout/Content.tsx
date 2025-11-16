88f04110f672aee1bbb765d004ce218bcf804047
Sebastian Krawiec <sebastian.arielk@gmail.com>
2025-11-16 17:51:10 -0300

    remove various

--- src/components/layout/Content.tsx
+++
@@ -1,9 +0,0 @@
-import type { ReactNode } from "preact/compat";
-
-type ContentProps = {
-  children: ReactNode;
-};
-
-export const Content = ({ children }: ContentProps) => {
-  return <div>{children}Content</div>;
-};
