3d099bb3b93ffedfc707f3d429edd64c7ee11c3f
Sebastian Krawiec <sebastian.arielk@gmail.com>
2025-11-16 17:57:22 -0300

    add navBar to site

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,18 @@
 import { Contacto, Inicio, Experiencia, Proyectos } from "./components/Pages";
 import "./app.scss";
+import { Nav } from "./components/layout";
 
 export function App() {
+  const navStyle = {
+    position: "sticky",
+    left: "67.5rem",
+    top: "3rem",
+  };
+
   return (
     <div className="app">
       <Inicio />
+      <Nav style={navStyle} />
       <Experiencia />
       <Proyectos />
       <Contacto />
