5f3998be46e556333570657ce64b55d4d180c018
Sebastian Krawiec <sebastian.arielk@gmail.com>
2025-11-19 18:46:27 -0300

    change proptype name

--- src/components/layout/Page.tsx
+++ src/components/layout/Page.tsx
@@ -1,13 +1,13 @@
 import type { ReactNode } from "preact/compat";
 import "../sass/_layoutCss/Page.scss";
 
-type ContentProps = {
+type PageProps = {
   children?: ReactNode;
   id?: string;
   extraClass?: string;
 };
 
-export const Page = ({ children, id, extraClass }: ContentProps) => {
+export const Page = ({ children, id, extraClass }: PageProps) => {
   return (
     <section className={`page ${extraClass}`} id={id}>
       {children}
