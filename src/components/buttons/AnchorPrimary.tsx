c63499f84bca2bb3a1d5ef20a2ecff6052f0d7dd
Sebastian Krawiec <sebastian.arielk@gmail.com>
2025-11-19 18:41:33 -0300

    add anchor wip

--- src/components/buttons/AnchorPrimary.tsx
+++ src/components/buttons/AnchorPrimary.tsx
@@ -5,11 +5,21 @@ type AnchorProps = {
   children?: ReactNode;
   goTo?: string;
   target?: string;
+  label: string;
+  icon?: ReactNode;
 };
 
-export default function AnchorPrimary({ children, goTo, target }: AnchorProps) {
+export default function AnchorPrimary({
+  children,
+  goTo,
+  target,
+  label,
+  icon,
+}: AnchorProps) {
   return (
     <a href={goTo} className="btn--anchor" target={target}>
+      {icon}
+      {label}
       {children}
     </a>
   );
