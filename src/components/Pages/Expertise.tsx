f1dfbe4cc17da8c28519949ae5cf7ce450530664
Sebastian Krawiec <sebastian.arielk@gmail.com>
2025-11-19 18:45:31 -0300

    add expr wip

--- src/components/Pages/Expertise.tsx
+++ src/components/Pages/Expertise.tsx
@@ -1,9 +1,27 @@
+import { HorizontalCard } from "../cards/Cards";
+import { Content } from "../layout";
 import { Page } from "../layout/Page";
+import "../sass/_pagesCss/_pagesExpertice.scss";
+import expertiseObj from "../../assets/expertise.json";
 
 export const Expertise = () => {
+  const { jobs } = expertiseObj;
+
   return (
-    <Page id="expertises" extraClass="exp">
-      Exp
+    <Page id="exp" extraClass="exp">
+      <Content extraClass="expertise-content">
+        {jobs.map(({ title, description, timeLapse }) => {
+          return (
+            <HorizontalCard
+              title={title}
+              time={timeLapse}
+              text={description}
+              alt="asd"
+              img="asd"
+            />
+          );
+        })}
+      </Content>
     </Page>
   );
 };
